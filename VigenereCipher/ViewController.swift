f1dcbfbab5fa6cd20a25c1ff0872d9347a2c2911
illescasDaniel <illescas.daniel@protonmail.com>
2018-03-10 16:13:34 +0100

    Initial Commit

---
+++ VigenereCipher/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  VigenereCipher
+//
+//  Created by Daniel Illescas Romero on 10/03/2018.
+//  Copyright © 2018 Daniel Illescas Romero. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: Any? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
